6a7dba98950fe42b51c039663733da49b2e621c6
Hannah Neal <hannahmneal@gmail.com>
2019-01-22 22:46:41 -0600

    Started practice exercise and resolved errors; Locations list says 'test'; still need to populated locations.

---
+++ src/components/Locations.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Locations extends Component {
+    render() {
+        return (
+            <h4>Test</h4>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
-import ReactDOM from 'react-dom';
-import React, { Component } from 'react';
+//================================      SINGLE COMPONENT       ==============================
 
-class Kennel extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Student Kennels</h3>
-                <h4>Nashville North Location</h4>
-                <h5>500 Puppy Way</h5>
-            </div>
-        );
-    }
-}
+import ReactDOM from "react-dom"
+import React from 'react'
+import Kennel from "./components/Kennel"
 
 ReactDOM.render(<Kennel />, document.querySelector("#root"));
+
+// Think of each one of your components as a factory function (i.e., a function that returns an object)
+//  extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h3>Kitten Kennels</h3>
+//                 <h4>Nashville West Location</h4>
+//                 <h5>500 Pouncing Path</h5>
+//                 <EmployeeList />    {/* This line signals that the Kennel component is a parent of the EmployeeList component. */}
+//             </div>
+//         );
+//     }
+// }
+
+//============================================================================================
+//================================      CHILD COMPONENT       ================================
+
+// A good developer adheres to the "Single Responsibility Principle"
+
+// class EmployeeList extends Component {
+//     render() {
+//         return (
+//             <article>
+//                 <h1>Employee List</h1>
+//                 <section>Jessica Younker</section>
+//                 <section>Jordan Nelson</section>
+//                 <section>Zoe LeBlanc</section>
+//                 <section>Blaise Roberts</section>
+//             </article>
+//         );
+//     }
+// }
+// ReactDOM.render(<Kennel />, document.querySelector("#root"));
