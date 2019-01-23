d0f89abaf2b12537798a5e7e5edc1c7cd6de8246
Hannah Neal <hannahmneal@gmail.com>
2019-01-23 14:28:37 -0600

    employee list and locations are displayed in the console; no styling elmeents are displayed

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -3,16 +3,56 @@ import React, { Component } from 'react';
 import EmployeeList from "../employees/EmployeeList"  // Import EmployeeList component
 import Locations from "./Locations" // Import EmployeeList component
 
-export default class Kennel extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Kitten Kennels</h3>
-                <h4>Nashville West Location</h4>
-                <h5>500 Pouncing Path</h5>
-                <EmployeeList />    {/*This line signals that the Kennel component is a parent of the EmployeeList component. */}
-                <Locations />
-            </div>
-        );
-    }
+// ================================================    PRACTICE 2      ===============================================
+    class Kennel extends Component {
+
+    /*
+        Although you will eventually be pulling your objects
+        from your json-server API, for this chapter, we're
+        faking it and just creating those arrays in the component
+        itself
+    */
+   employeesFromAPI = [
+    { id: 1, name: "Jessica Younker" },
+    { id: 2, name: "Jordan Nelson" },
+    { id: 3, name: "Zoe LeBlanc" },
+    { id: 4, name: "Blaise Roberts" }
+]
+
+// This will eventually get pulled from the API
+locationsFromAPI = [
+    { id: 1, name: "Nashville North", address: "3749 Little Paws Parkway" },
+    { id: 2, name: "Nashville South", address: "23098 Whiskers Way" }
+]
+
+state = {
+    employees: this.employeesFromAPI,
+    locations: this.locationsFromAPI
 }
+render() {
+    return (
+        <article className="kennel">
+            <Locations locations={this.state.locations} />
+            <EmployeeList employees={this.state.employees} />
+        </article>
+    )
+}
+}
+
+export default Kennel
+// ================================================    PRACTICE 1      ===============================================
+
+// export default class Kennel extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h3>Kitten Kennels</h3>
+//                 <h4>Nashville West Location</h4>
+//                 <h5>500 Pouncing Path</h5>
+//                 <EmployeeList />    {/*This line signals that the Kennel component is a parent of the EmployeeList component. */}
+//                 <Locations />
+//             </div>
+//         );
+//     }
+// }
+// =======================================================================================================================
--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react'
+// ================================================    PRACTICE 2      ===============================================
 
-export default class Locations extends Component {
+class Locations extends Component {
     render() {
         return (
             <article>
-            <h2>Test</h2>
-                <section>Nashville North</section>
-                <p>3749 Little Paws Parkway</p>
-                <section>Nashville South</section>
-                <p>23098 Whiskers Way</p>
+            <h2>Other Locations</h2>
+                <section className = "other-locations">
+                { /* If you do not use "this." before "props", you will get an error message that says "failed to define..." */
+                    this.props.locations.map(responseLocation =>
+                        <div key={responseLocation.id}>
+                            {responseLocation.name}
+                        </div>
+                    )
+                }
+                </section>
+                {/* </br> */}
             </article>
         );
     }
 }
+
+export default Locations
+
+
+// ================================================    PRACTICE 1      ===============================================
+
+// export default class Locations extends Component {
+//     render() {
+//         return (
+//             <article>
+//             <h2>Test</h2>
+//                 <section>Nashville North</section>
+//                 <p>3749 Little Paws Parkway</p>
+//                 <section>Nashville South</section>
+//                 <p>23098 Whiskers Way</p>
+//             </article>
+//         );
+//     }
+// }
 
