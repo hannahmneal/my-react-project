d5da36382030295ee00f2d8fab133f0684de3e59
Hannah Neal <hannahmneal@gmail.com>
2019-01-22 23:20:14 -0600

    resolved errors and finished project

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react'
 export default class Locations extends Component {
     render() {
         return (
-            <h4>Test</h4>
+            <article>
+            <h2>Test</h2>
+                <section>Nashville North</section>
+                <p>3749 Little Paws Parkway</p>
+                <section>Nashville South</section>
+                <p>23098 Whiskers Way</p>
+            </article>
         );
     }
 }
+
