9e582651edd5064b3446967ec45a5565ffef7520
Hannah Neal <hannahmneal@gmail.com>
2019-01-24 00:37:30 -0600

    completed setup but did not resolve errors

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -9,10 +9,10 @@ class NavBar extends Component {
             <nav className="navbar navbar-light fixed-top light-blue flex-md-nowrap p-0 shadow">
                 <ul className="nav nav-pills">
                     <li className="nav-item">
-                        <Link className="nav-link" to="/">Locations</Link>
+                        <Link className="nav-link" to="/animals">Animals</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/animals">Animals</Link>
+                        <Link className="nav-link" to="/">Locations</Link>
                     </li>
                     <li className="nav-item">
                         <Link className="nav-link" to="/employees">Employees</Link>
